47c023bb5625522c0cb66e4287d2f5d652ed903a
gzhggg <949908131@qq.com>
2020-06-09 21:59:54 +0800

    commit

---
+++ ArticleToCategoryService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlogSystem.Models;
+
+namespace BlogSystem.DAL
+{
+    public class ArticleToCategoryService : BaseService<Models.ArticleToCategory>, IDAL.IArticleToCategoryService
+    {
+        public ArticleToCategoryService() : base(new BlogContext())
+        {
+        }
+    }
+}
