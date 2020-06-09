cf133720356ce2e58f71a398d0d99d07345c5a76
gzhggg <949908131@qq.com>
2020-06-09 21:45:08 +0800

    commit

---
+++ ArticleManager.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlogSystem.Dto;
+
+namespace BlogSystem.BLL
+{
+    public class ArticleManager : IBLL.IArticleManager
+    {
+        public async Task BadCount(Guid articleId)
+        {
+            using (var articleSvc = new DAL.ArticleService())
+            {
+                var data = await articleSvc.GetOneByIdAsync(articleId);
+                data.BadCount += 1;
+                await articleSvc.EditAsync(data);
+            }
+        }
+
+        public async Task CreateArticle(string title, string content, Guid[] categoryIds, Guid userId)
+        {
+            using (IDAL.IArticleService articleSvc = new DAL.ArticleService())
+            {
+                var article = new Models.Article()
+                {
+                    Title = title,
+                    Content = content,
+                    UserId = userId
+                };
+                await articleSvc.CreateAsync(article);
+
+                Guid articleId = article.Id;
+                using (var articleToCategroySvc = new DAL.ArticleToCategoryService())
+                {
+                    foreach (var categoryId in categoryIds)
+                    {
+                        await articleToCategroySvc.CreateAsync(new Models.ArticleToCategory()
+                        {
+                            ArticleId = articleId,
+                            BlogCategoryId = categoryId
+                        }, saved: false);
+                    }
+                    await articleToCategroySvc.Save();
+                }
+            }
+        }
+
+        public async Task CreateCategory(string name, Guid userId)
+        {
+            using (IDAL.IBlogCategory categorySvc = new DAL.BlogCategoryService())
+            {
+                await categorySvc.CreateAsync(new Models.BlogCategory()
+                {
+                    CategoryName = name,
+                    UserId = userId
+                });
+            }
+        }
+
+        public async Task EditArticle(Guid articleId, string title, string content, Guid[] categoryIds)
+        {
+            using (var articleSvc = new DAL.ArticleService())
+            {
+                var article = await articleSvc.GetOneByIdAsync(articleId);
+                article.Title = title;
+                article.Content = content;
+                await articleSvc.EditAsync(article);
+            }
+            using (var articleToCategorySvc = new DAL.ArticleToCategoryService())
+            {
+                foreach (var item in articleToCategorySvc.GetAllAsync().Where(m=>m.ArticleId==articleId))
+                {
+                    await articleToCategorySvc.RemoveAsync(item, false);
+                }
+                foreach (var item in categoryIds)
+                {
+                    await articleToCategorySvc.CreateAsync(new Models.ArticleToCategory() { ArticleId = articleId, BlogCategoryId = item }, false);
+                }
+                await articleToCategorySvc.Save();
+            }
+        }
+
+        public async Task EditCategory(Guid categoryId, string newcategoryName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> ExistsArticle(Guid articleId)
+        {
+            using (var articleSvc = new DAL.ArticleService())
+            {
+                return await articleSvc.GetAllAsync().AnyAsync(m => m.Id == articleId);
+            }
+        }
+
+        public async Task<List<ArticleDto>> GetAllArticlesByCategoryId(Guid categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<ArticleDto>> GetAllArticlesByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<ArticleDto>> GetAllArticlesByUserId(Guid userId, int pageIndex, int pageSize)
+        {
+            using (var articleSvc = new DAL.ArticleService())
+            {
+                var list = await articleSvc.GetAllByPageOrderAsync(pageSize, pageIndex, false).Include(m => m.User).Where(m => m.UserId == userId).Select(m => new ArticleDto()
+                {
+                    Id = m.Id,
+                    Title = m.Title,
+                    Content = m.Content,
+                    CreateTime = m.CreateTime,
+                    GoodCount = m.GoodCount,
+                    BadCount = m.BadCount,
+                    Email = m.User.Email,
+                    ImagePath = m.User.ImagePath
+                }).ToListAsync();
+
+                using (var articleToCategroySvc = new DAL.ArticleToCategoryService())
+                {
+                    foreach (var item in list)
+                    {
+                        var articleToCategroyData = articleToCategroySvc.GetAllAsync().Include(m => m.BlogCategory).Where(m => m.ArticleId == item.Id);
+                        item.CategoryIds = articleToCategroyData.Select(m => m.BlogCategoryId).ToArray();
+                        item.CategoryNames = articleToCategroyData.Select(m => m.BlogCategory.CategoryName).ToArray();
+                    }
+                    return list;
+                }
+            }
+        }
+
+        public async Task<List<BlogCategoryDto>> GetAllCategories(Guid userId)
+        {
+            using (IDAL.IBlogCategory categorySvc = new DAL.BlogCategoryService())
+            {
+                return await categorySvc.GetAllAsync().Where(m => m.UserId == userId).Select(m => new BlogCategoryDto()
+                {
+                    Id = m.Id,
+                    CategoryName = m.CategoryName
+                }).ToListAsync();
+            }
+        }
+
+        public async Task<int> GetDataCount(Guid userId)
+        {
+            using (var articleSvc = new DAL.ArticleService())
+            {
+                return await articleSvc.GetAllAsync().CountAsync(m => m.UserId == userId);
+            }
+        }
+
+        public async Task<ArticleDto> GetOneArticleById(Guid articleId)
+        {
+            using (var articleSvc = new DAL.ArticleService())
+            {
+                var data = await articleSvc.GetAllAsync().Include(m => m.User).Where(m => m.Id == articleId).Select(m => new Dto.ArticleDto()
+                {
+                    Id = m.Id,
+                    Title = m.Title,
+                    Content = m.Content,
+                    CreateTime = m.CreateTime,
+                    GoodCount = m.GoodCount,
+                    BadCount = m.BadCount,
+                    Email = m.User.Email,
+                    ImagePath = m.User.ImagePath
+                }).FirstAsync();
+
+                using (var articleToCategroySvc = new DAL.ArticleToCategoryService())
+                {
+                    var articleToCategroyData = articleToCategroySvc.GetAllAsync().Include(m => m.BlogCategory).Where(m => m.ArticleId == data.Id);
+                    data.CategoryIds = articleToCategroyData.Select(m => m.BlogCategoryId).ToArray();
+                    data.CategoryNames = articleToCategroyData.Select(m => m.BlogCategory.CategoryName).ToArray();
+
+                    return data;
+                }
+            }
+        }
+
+        public async Task GoodCount(Guid articleId)
+        {
+            using (var articleSvc = new DAL.ArticleService())
+            {
+                var data = await articleSvc.GetOneByIdAsync(articleId);
+                data.GoodCount += 1;
+                await articleSvc.EditAsync(data);
+            }
+        }
+
+        public async Task RemoveArticle(Guid articleId)
+        {
+            using (var articleSvc = new DAL.ArticleService())
+            {
+                await articleSvc.RemoveAsync(articleId);
+            }
+
+        }
+
+        public async Task RemoveCategory(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UserManager.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlogSystem.Dto;
+using BlogSystem.IBLL;
+using BlogSystem.Models;
+
+namespace BlogSystem.BLL
+{
+    public class UserManager : IUserManager
+    {
+        public async Task ChangePassword(string email, string oldPwd, string newPwd)
+        {
+            using (IDAL.IUserService userSvc = new DAL.UserService())
+            {
+                if (await userSvc.GetAllAsync().AnyAsync(m => m.Email == email && m.Password == oldPwd))
+                {
+                    var user = await userSvc.GetAllAsync().FirstAsync(m => m.Email == email);
+                    user.Password = newPwd;
+                    await userSvc.EditAsync(user);
+                }
+            }
+        }
+
+        public async Task ChangeUserInformation(string email, string siteName, string imagePath)
+        {
+            using (IDAL.IUserService userSvc = new DAL.UserService())
+            {
+                var user = await userSvc.GetAllAsync().FirstAsync(m => m.Email == email);
+                user.SiteName = siteName;
+                user.ImagePath = imagePath;
+                await userSvc.EditAsync(user);
+            }
+        }
+
+        public async Task<UserInformationDto> GetUserByEmail(string email)
+        {
+            using (IDAL.IUserService userSvc = new DAL.UserService())
+            {
+                if (await userSvc.GetAllAsync().AnyAsync(m => m.Email == email))
+                {
+                    return await userSvc.GetAllAsync().Where(m => m.Email == email).Select(m => new UserInformationDto()
+                    {
+                        Id = m.Id,
+                        Email = m.Email,
+                        ImagePath = m.ImagePath,
+                        FansCount = m.FansCount,
+                        SiteName = m.SiteName,
+                        FocusCount = m.FocusCount
+                    }).FirstAsync();
+                }
+                else
+                {
+                    throw new ArgumentException("邮箱地址不存在");
+                }
+            }
+        }
+
+        public bool Login(string email, string password,out Guid userId)
+        {
+            using (IDAL.IUserService userSvc = new DAL.UserService())
+            {
+                var user = userSvc.GetAllAsync().FirstOrDefaultAsync(m => m.Email == email && m.Password == password);
+                user.Wait();
+                var data = user.Result;
+                if (data == null)
+                {
+                    userId = new Guid();
+                    return false;
+                }
+                else
+                {
+                    userId = data.Id;
+                    return true;
+                }
+            }
+        }
+
+        public async Task Register(string email, string password)
+        {
+            using (IDAL.IUserService userSvc = new DAL.UserService())
+            {
+                await userSvc.CreateAsync(new User()
+                {
+                    Email = email,
+                    Password = password,
+                    ImagePath = "default.png",
+                    SiteName = "默认的博客"
+                });
+            }
+        }
+    }
+}
